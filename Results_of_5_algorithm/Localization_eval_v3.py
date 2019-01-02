cb7020195c5ad739af500a8ed8566b355de0ee0e
Nasreen Mohsin <33446075+nasreenpmohsin@users.noreply.github.com>
2019-01-02 10:03:08 -0800

    Update

--- Results_of_5_algorithm/Localization_eval_v3.py
+++ Results_of_5_algorithm/Localization_eval_v3.py
@@ -2,9 +2,10 @@
 Evaluation version 3
 Technique 1: Weighted Least Squares estimation through hyperbolic technique
 Author: Nasreen Mohsin
-USAGE= python SFU_Localization_eval_v5.py TX MAX_NUM_pkts offline read_data.txt Position_no. sv save_file.txt
-       python SFU_localization_eval_v5.py TX MAX_NUM_pkts online IP_addr Port  sv save_file.txt
-Eg: python SFU_Localization_eval_v5.py -8 5 offline data_eval_pos2.txt 2  sv eval_v5_pos2.txt
+WDW_SIZE=[3,6,9,12,15,18]
+USAGE= python Localization_eval_v3.py TX WDW_SIZE offline read_data.txt Position_no. sv save_file.txt
+       python Localization_eval_v3.py TX WDW_SIZE online IP_addr Port  sv save_file.txt
+Eg: python Localization_eval_v3.py -8 6 offline data_eval_pos2.txt 2  sv eval_v5_pos2.txt
 '''
 
 import os, sys, time
